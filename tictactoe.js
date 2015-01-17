28dc923a550c43e200bb4f28a7ac7a4c0a1affc8
Erik Bylund <ebylundini@gmail.com>
2015-01-17 10:25:05 -0700

    add set up logic to complete winner feature

--- tictactoe.js
+++ tictactoe.js
@@ -1,28 +1,37 @@
-  var moves = true, gameStatus = false, winSet = [];
+  var moves = true, gameStatus = false, winSetX = [], winSetO = [];
   var clickHandler = function() {
     if (!gameStatus && this.innerHTML == '&nbsp;') {
       this.innerHTML = moves ? 'X' : 'O';
-      winSet.push(this.id);
-      console.log(winSet);
+      if (this.innerHTML == 'X'){
+        winSetX.push(this.id);
+        winDetect(winSetX);
+      }
+      else{
+        winSetO.push(this.id);
+        winDetect(winSetO);
+      }
       moves = !moves;
     }
   };
-  var i, tagList = document.getElementsByTagName('td');
-  for (i = 0; i < tagList.length; i++) {
+  var tagList = document.getElementsByTagName('td');
+  var winDetect = function(testArray){
+    var j = 0, t = 0;
+    var winningKey = [
+      // rows
+      ["00", "10", "20"], ["01", "11", "21"], ["02", "12", "22"],
+      // columns
+      ["00", "01", "02"], ["10", "11", "12"], ["20", "21", "22"],
+      // diagonal
+      ["20", "11", "02"], ["00", "11", "22"]
+    ];
+    for (j = 0; j < winningKey.length; i++){
+      for (t = 0; t < winningKey[j].length; i++){
+        //winningKey[j][t]
+      }
+    }
+  };
+
+  for (var i = 0; i < tagList.length; i++) {
     tagList[i].onclick = clickHandler;
   };
-  var winDetect = function(){
-    var winningKey = [
-   // rows
-      ["square00", "square10", "square20"],
-      ["square01", "square11", "square21"],
-      ["square02", "square12", "square22"],
-    // columns
-      ["square00", "square01", "square02"],
-      ["square10", "square11", "square12"],
-      ["square20", "square21", "square22"],
-    // diagonal
-      ["square20", "square11", "square02"],
-      ["square00", "square11", "square22"]
-    ];
-  };
+
