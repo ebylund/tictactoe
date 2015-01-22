c234c24cfe64b6cd066abb4bc10098c41711140a
Erik Bylund <ebylundini@gmail.com>
2015-01-21 23:44:18 -0700

    add start logic to board

--- tictactoe.js
+++ tictactoe.js
@@ -9,81 +9,26 @@ var winningKey = [
   ["square20", "square11", "square02"], ["square00", "square11", "square22"]
 ];
 
-var currentBoard = [
-  0,0,0,
-  0,0,0,
-  0,0
-];
-
-var j = 0, t = 0, r = 0, i = 0, p = 0, q = 0;
+var  i = 0, j = 0;
 var winningList = [];
 
 var clickHandler = function() {
   if (!gameStatus && this.innerHTML == '&nbsp;') {
     this.innerHTML = moves ? 'X' : 'O';
-    if (this.innerHTML == 'X'){
-      winSetX.push(this.id);
-      winDetect(winSetX, this);
-    }
-    else{
-      winSetO.push(this.id);
-      winDetect(winSetO, this);
-    }
+    winDetect(this);
     moves = !moves;
   }
 };
 
-var winDetect = function(testArray, that){
-  for (j = 0; j < testArray.length; j++){
-    for (t = 0; t < winningKey.length; t++){
-      for (r = 0; r < winningKey[t].length; r++){
-        if (testArray[j] == winningKey[t][r] && testArray[j] == that.id){
-          if (that.innerHTML == 'X'){
-            currentBoard[t] += 1;
-          }
-          else if(that.innerHTML == 'O'){
-            currentBoard[t] -= 1;
-          }
-        }
-      }
+var winDetect = function(that){
+  for (i = 0; i < winningKey.length; i++){
+    if (winningKey[i][0] == 'X'){
+      console.log('winner man!');
     }
   }
-
-  // console.log('X board:' + currentBoard);
-  for (i = 0; i < currentBoard.length; i++) {
-
-    if (currentBoard[i] == 3){
-      console.log(currentBoard);
-      winningList.push(i);
-    }
-    if (currentBoard[i] == -3){
-      winningList.push(i);
-    }
-
-    if (winningList.length > 0){
-      // highlight(winningList);
-    }
-  };
 };
 
-var highlight = function(winningList) {
-  for (q = 0; q < winningList.length; q++){
-    // console.log(winningKey[q]);
-    // for (p = 0; p < winningKey[q]; p++){
-    //   console.log(winningKey[p]);
-    // }
-    
-  }
-  // for (p = 0; p < winningKey.length; p++){
-  //   for (q = 0; q < winningKey[t].length; q++){
-
-  //   }
-  // }
-  // alert('Game Over!');
-}
-
 var tagList = document.getElementsByTagName('td');
-
 for (var i = 0; i < tagList.length; i++) {
   tagList[i].onclick = clickHandler;
 };
